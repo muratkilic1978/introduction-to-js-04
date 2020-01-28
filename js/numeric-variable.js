c4698942141edd920450388ce503bfb97a6a2c7f
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-28 17:24:46 +0100

    script.js was updated with solution

--- js/numeric-variable.js
+++ js/numeric-variable.js
@@ -1,1 +1,16 @@
+//Create three variables to store infomation needed
+let price, quantity, total;
+
+// fetch DOM element that has the ID attribute "cost"
+const costElem = document.getElementById("cost");//
+
+//Assign values to the price and quantity variables
+price = 99;
+quantity = 4;
+
+//Calculate the price
+total = price * quantity;
+
+//Update the DOM element that has the ID attribute "cost" and insert the total price 
+costElem.textContent = '$'+total;
 
